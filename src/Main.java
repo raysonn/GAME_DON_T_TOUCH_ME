873c087892fbc1440993945d72a5608e9e02e4f0
raysonn taborda <raysonn.taborda@ewave.com.br>
2019-11-20 11:24:00 -0300

    Clear

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 
 public class Main {
 
-	private static Painel painel;
-	private static Temporizador temporizador;
+    private static Painel painel;
+    private static Temporizador temporizador;
 
-	public static void main(String args[]) {
-		painel = new Painel();
-		temporizador = new Temporizador();
-		painel.quadro(null, null);
-		temporizador.run();
-	}
+    public static void main(String args[]) {
+        painel = new Painel();
+        temporizador = new Temporizador();
+        painel.quadro();
+        temporizador.run();
+    }
 }
--- src/Painel.java
+++ src/Painel.java
@@ -8,79 +8,79 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Painel extends JPanel {
-	private static final long serialVersionUID = 1L;
-	private static Animacao animacao;
-	private static JFrame janela;
-	private static Painel painel;
-	private static Temporizador temporizador;
-	private int x;
-	private int y;
-	private int initialX;
-	private int initialY;
-	private int finalX;
-	private int finalY;
-	private static int acertos = 0;
-	private static int erros = 0;
+    private static final long serialVersionUID = 1L;
+    private static Animacao animacao;
+    private static JFrame janela;
+    private static Painel painel;
+    private static Temporizador temporizador;
+    private int x;
+    private int y;
+    private int initialX;
+    private int initialY;
+    private int finalX;
+    private int finalY;
+    private static int acertos = 0;
+    private static int erros = 0;
 
-	public void paint(Graphics g) {
-		animacao = new Animacao();
-		super.paint(g);
-		Graphics2D gr = (Graphics2D) g;
-		animacao.paint(gr, g);
-		repaint();
-	}
+    public void paint(Graphics g) {
+        animacao = new Animacao();
+        super.paint(g);
+        Graphics2D gr = (Graphics2D) g;
+        animacao.paint(gr, g);
+        repaint();
+    }
 
-	public void quadro(Graphics2D gr, Graphics g){
-		janela = new JFrame();
-		painel = new Painel();
-		painel.setFocusable(true);
-		janela.setVisible(true);
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void quadro() {
+        janela = new JFrame();
+        painel = new Painel();
+        painel.setFocusable(true);
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		janela.setSize(800, 600);
-		janela.add(painel);
-		janela.setTitle("DON'T TOUCH ME 'GAME'");
-		janela.addMouseListener(new MouseAdapter() {
-			public void mouseReleased(MouseEvent click) {
-				detectClick(click);
-			}
-		});
-	}
+        janela.setSize(800, 600);
+        janela.add(painel);
+        janela.setTitle("DON'T TOUCH ME 'GAME'");
+        janela.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent click) {
+                detectClick(click);
+            }
+        });
+    }
 
-	private void detectClick(MouseEvent click) {
-		temporizador = new Temporizador();
-		janela.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
-		x = click.getX() - 300;
-		y = click.getY() - 300;
-		initialX = animacao.getPosicaoX();
-		initialY = animacao.getPosicaoY();
-		finalX = 7 + initialX + animacao.getWidth();
-		finalY = 25 + initialY + animacao.getHeight();
-		janela.setTitle("D'ONT TOUCH ME 'GAME'" + " X: " + x + " Y: " + y);
-		if ((x > initialX && x < finalX) && (y > initialY && y < finalY)) {
-			acertos++;
-			temporizador.setSegundo(temporizador.getSegundo()+1);
-		}
-		if ((x < initialX || x > finalX) || (y < initialY || y > finalY)) {
-			erros++;
-			temporizador.setSegundo(temporizador.getSegundo()-5);
-		}
-	}
-	
-	public int getAcertos() {
-		return acertos;
-	}
+    private void detectClick(MouseEvent click) {
+        temporizador = new Temporizador();
+        janela.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+        x = click.getX() - 300;
+        y = click.getY() - 300;
+        initialX = animacao.getPosicaoX();
+        initialY = animacao.getPosicaoY();
+        finalX = 7 + initialX + animacao.getWidth();
+        finalY = 25 + initialY + animacao.getHeight();
+        janela.setTitle("D'ONT TOUCH ME 'GAME'" + " X: " + x + " Y: " + y);
+        if ((x > initialX && x < finalX) && (y > initialY && y < finalY)) {
+            acertos++;
+            temporizador.setSegundo(temporizador.getSegundo() + 1);
+        }
+        if ((x < initialX || x > finalX) || (y < initialY || y > finalY)) {
+            erros++;
+            temporizador.setSegundo(temporizador.getSegundo() - 5);
+        }
+    }
 
-	public void setAcertos(int acertos) {
-		Painel.acertos = acertos;
-	}
+    public int getAcertos() {
+        return acertos;
+    }
 
-	public int getErros() {
-		return erros;
-	}
+    public void setAcertos(int acertos) {
+        Painel.acertos = acertos;
+    }
 
-	public void setErros(int erros) {
-		Painel.erros = erros;
-	}
+    public int getErros() {
+        return erros;
+    }
+
+    public void setErros(int erros) {
+        Painel.erros = erros;
+    }
 
 }
